5fa8619502ffccfb9f6313c3a02455f26faede2e
volca <volca@tom.com>
2016-11-30 11:09:25 +0800

    reformat code

--- libraries/CactusMicro/examples/temperatureLogger/readme.md
+++ libraries/CactusMicro/examples/temperatureLogger/readme.md
@@ -1,1 +1,6 @@
 Update version for [temperature logger](http://www.instructables.com/id/Arduino-Wifi-Temperature-Logger/). This sketch can work with esp8266 espduino firmware together
+
+## How To Start
+
+* Install libraries DallasTemperature, OneWire by "Library Manager". 
+* Upload the sketch
--- libraries/CactusMicro/examples/temperatureLogger/temperatureLogger.ino
+++ libraries/CactusMicro/examples/temperatureLogger/temperatureLogger.ino
@@ -1,9 +1,9 @@
 /**
- * \file
- *       Temperature logger example
- * \author
- *       Yan <yancan@aprbrother.com>
- */
+   \file
+         Temperature logger example
+   \author
+         Yan <yancan@aprbrother.com>
+*/
 
 #include <espduino.h>
 #include <rest.h>
@@ -11,6 +11,10 @@
 #include <DallasTemperature.h>
 
 #define PIN_ENABLE_ESP 13
+
+//pin for the DS18B20 temperature module
+#define TEMPERATURE_PIN 14
+
 #define SSID  "YOUR-SSID"
 #define PASS  "YOUR-WIFI-PASS"
 
@@ -20,7 +24,7 @@
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature sensors(&oneWire);
 
-// use http://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html 
+// use http://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html
 // to find the address of your DS18B20, it will be different then this
 DeviceAddress thermometer = { 0x28, 0x60, 0xB7, 0xCC, 0x04, 0x00, 0x00, 0x76 };
 
@@ -35,71 +39,71 @@ void wifiCb(void* response)
   uint32_t status;
   RESPONSE res(response);
 
-  if(res.getArgc() == 1) {
+  if (res.getArgc() == 1) {
     res.popArgs((uint8_t*)&status, 4);
-    if(status == STATION_GOT_IP) {
+    if (status == STATION_GOT_IP) {
       Serial.println("WIFI CONNECTED");
-     
+
       wifiConnected = true;
     } else {
       wifiConnected = false;
     }
-    
+
   }
 }
 
 void setup() {
-	Serial1.begin(19200);
-	Serial.begin(19200);
+  Serial1.begin(19200);
+  Serial.begin(19200);
 
-	sensors.begin();
+  sensors.begin();
 
-	esp.enable();
-	delay(500);
-	esp.reset();
-	delay(500);
-	while(!esp.ready());
+  esp.enable();
+  delay(500);
+  esp.reset();
+  delay(500);
+  while (!esp.ready());
 
-	Serial.println("ARDUINO: setup rest client");
-		if(!rest.begin("data.sparkfun.com")) {
-		Serial.println("ARDUINO: failed to setup rest client");
-		while(1);
-	}
+  Serial.println("ARDUINO: setup rest client");
+  if (!rest.begin("data.sparkfun.com")) {
+    Serial.println("ARDUINO: failed to setup rest client");
+    while (1);
+  }
 
-	/*setup wifi*/
-	Serial.println("ARDUINO: setup wifi");
-	esp.wifiCb.attach(&wifiCb);
+  /*setup wifi*/
+  Serial.println("ARDUINO: setup wifi");
+  esp.wifiCb.attach(&wifiCb);
 
-	esp.wifiConnect(SSID, PASS);
-	Serial.println("ARDUINO: system started");
+  esp.wifiConnect(SSID, PASS);
+  Serial.println("ARDUINO: system started");
 }
 
 void loop() {
   char response[266];
   esp.process();
-  if(wifiConnected) {
-		char buff[200];
+  if (wifiConnected) {
+    char buff[200];
+    char temp[10];
+    float temperature;
 
-		//get temp
-		sensors.requestTemperatures();
+    //get temp
+    sensors.requestTemperatures();
+    temperature = sensors.getTempC(thermometer);
+    Serial.print("temp: ");
+    Serial.println(temperature);
+    dtostrf(temperature, 1, 2, temp);
 
-		//send temp
-		char temp[10];
+    //send temp
+    sprintf(buff, "/input/%s?private_key=%s&temp=%s", PUBLIC_KEY, PRIVATE_KEY, temp);
+    Serial.println(buff);
 
-		Serial.print("temp: ");     
-		Serial.println(temperature);
+    rest.get((const char*)buff);
+    Serial.println("ARDUINO: send get");
 
-		dtostrf(temperature,1,2,temp);
-		sprintf(buff, "/input/%s?private_key=%s&temp=%s", PUBLIC_KEY, PRIVATE_KEY, temp);
-        Serial.println(buff);
-
-        rest.get((const char*)buff);
-        Serial.println("ARDUINO: send get");
-
-        if(rest.getResponse(response, 266) == HTTP_STATUS_OK){
-            Serial.println("ARDUINO: GET successful");
-            Serial.println(response);
-        }
-        delay(30000);
+    if (rest.getResponse(response, 266) == HTTP_STATUS_OK) {
+      Serial.println("ARDUINO: GET successful");
+      Serial.println(response);
+    }
+    delay(30000);
   }
 }
